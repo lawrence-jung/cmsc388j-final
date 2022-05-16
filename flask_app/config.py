f1aa74a986803259989edc57cc78d4580389f4b6
lawrence-jung <junglawrence.c137@gmail.com>
2022-05-15 22:27:26 -0400

    prepared for deployment

--- flask_app/config.py
+++ flask_app/config.py
@@ -1,3 +1,3 @@
 # Stores all configuration values
 SECRET_KEY = b'\x020;yr\x91\x11\xbe"\x9d\xc1\x14\x91\xadf\xec'
-MONGODB_HOST = "mongodb://localhost:27017/final_proj"
+MONGODB_HOST = "mongodb+srv://admin-84:Wevniowevnio1@cmsc388j-finalproj.fgdvo.mongodb.net/myDatabase?retryWrites=true&w=majority"
